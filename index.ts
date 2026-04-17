5027bc672047ed4efb20fd7759c88ce1ebe5ad2a
nazozokc <nazozokc@gmail.com>
2026-04-17 16:57:06 +0900

    edit

--- index.ts
+++ index.ts
@@ -1,1 +1,27 @@
-console.log("Hello via Bun!");
+import { consola } from "consola";
+import { Command } from "commander";
+import { homedir } from "os";
+import { execa, execaSync } from "execa";
+import { readdir } from "fs";
+
+const runCLI = () => {
+  const program = new Command();
+
+  program.name("skills").description("skills list CLI").version("0.1.0");
+
+  program.command("claude").action(async () => {
+    try {
+      const files = await readdir(`${homedir}/.claude/skills`);
+
+      consola.log("claude code skills list");
+      for (const file of files) {
+        consola.log(file);
+      }
+    } catch (error) {}
+  });
+
+  program.parse();
+};
+
+runCLI();
+
